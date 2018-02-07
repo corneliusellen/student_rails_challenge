36bc1c8f3f65d193e2ac7f173a9ef61eccb5dcf4
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-07 13:56:50 -0700

    Add new and create paths in student controller. Add create form and new view. Passing test.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,21 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  def student_params
+    params.require(:student).permit(:name)
+  end
 end
